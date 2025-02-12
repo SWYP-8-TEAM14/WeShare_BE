0241901f37a406b092777c7722451acb63df9510
jiwon hwang <realjiwon98@naver.com>
2025-02-12 18:44:01 +0900

    config/urls.py에 swagger url 추가

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from apps.users.views import SignupView, LoginView
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login"),
+
+    # sers
+    path("api/v1/users/", include("apps.users.urls")),
+
+    # Swagger 및 API 문서
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
