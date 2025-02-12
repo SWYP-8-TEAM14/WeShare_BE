97c9c850fef17341328882e5515a010b392a162a
jiwon hwang <realjiwon98@naver.com>
2025-02-12 18:38:25 +0900

    auth_service.py -> 인증 관련 로직 분리

---
+++ apps/users/services/auth_service.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import authenticate
+from apps.users.models import User
+
+class AuthService:
+    @staticmethod
+    def authenticate_user(email, password):
+        user = authenticate(email=email, password=password)
+        if user and user.is_active:
+            return user
+        return None
