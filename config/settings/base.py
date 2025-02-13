776e554198b73fb293884472b9c4d24e8899b2b7
jiwon hwang <realjiwon98@naver.com>
2025-02-13 20:31:35 +0900

    ci에 SECRET_KEY 환경 변수 설정 delete, base.py에 .env load

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,10 +1,10 @@
+import os
 from pathlib import Path
 
-# from dotenv import load_dotenv
-# import os
-#
-# load_dotenv()  # 🔥 `.env` 파일 로드
-# SECRET_KEY = os.getenv("SECRET_KEY")
+from dotenv import load_dotenv
+
+load_dotenv()  # 🔥 `.env` 파일 로드
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
