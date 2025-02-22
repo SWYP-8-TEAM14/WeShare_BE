8dc3470986616954c0c0294085a9439e709c1408
Minhyeok Kang <santa4246@naver.com>
2025-02-22 23:29:56 +0900

    Feat: 공유물품 serialiizers 수정

--- apps/shared/serializers.py
+++ apps/shared/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
 from .models import User, Item, Reservation, RentalRecord
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = [
+                'group_id'
+                , 'group_name'
+                , 'item_id'
+                , 'item_name'
+                , 'item_image'
+                , 'quantity'
+                , 'created_at'
+                , 'is_wishlist'
+                , 'status'
+                , 'reservation_user_id'
+                , 'reservation_user_name'
+        ]
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
