5d26ab2d4235fb9f958b6f11897764edcbf2055f
Minhyeok Kang <santa4246@naver.com>
2025-02-22 22:47:59 +0900

    Feat: 물품관리 models 업데이트

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -11,18 +11,26 @@ class Item(models.Model):
     quantity = models.IntegerField(default=1)
     caution = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now_add=True)
 
 class Reservation(models.Model):
-    book_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    book_date = models.DateTimeField(auto_now_add=True)
+    reservation_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    reservation_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50)  # '예약 완료', '취소됨' 등
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class RentalRequest(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    requested_at = models.DateTimeField(auto_now_add=True)
 
 class RentalRecord(models.Model):
     rental_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
     rental_start = models.DateTimeField()
     rental_end = models.DateTimeField()
     actual_return = models.DateTimeField(null=True, blank=True)
