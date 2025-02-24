7f042355a4b87793072a9fc9778edc66a539a0ae
benchprincess <realjiwon98@naver.com>
2025-02-24 19:18:01 +0900

    group 상세 페이지에서 그룹관리 (그룹장만 가능) 그룹 상세, 관리페이지에서 공용 물품 리스트 가져오는 로직추가

--- apps/groups/urls.py
+++ apps/groups/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import (
-    GroupListView, GroupDetailView, GroupCreateView
+    GroupListView, GroupDetailView, GroupCreateView, GroupManageView, GroupMemberListView, GroupSharedItemListView
 )
 
 
 urlpatterns = [
     path("groups/", GroupListView.as_view(), name="group-list"),
     path("groups/<int:group_id>/", GroupDetailView.as_view(), name="group-detail"),
+    path("groups/<int:group_id>/manage/", GroupManageView.as_view(), name="group-manage"),
+    path("groups/<int:group_id>/members/", GroupMemberListView.as_view(), name="group-member-list"),
+    path("groups/<int:group_id>/items/", GroupSharedItemListView.as_view(), name="group-shared-item-list"),
     path("groups/create/", GroupCreateView.as_view(), name="group-create"),
 ]
--- apps/groups/views.py
+++ apps/groups/views.py
@@ -5,8 +5,12 @@ from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
 from .models import Group, GroupMember
 from .serializers import GroupSerializer, GroupCreateSerializer, GroupMemberSerializer
+from ..shared.models import Item
+from ..shared.serializers import ItemSerializer
 
 
 class GroupListView(generics.ListAPIView):
@@ -49,15 +53,91 @@ class GroupDetailView(generics.RetrieveAPIView):
         except Group.DoesNotExist:
             return Response({"message" : "해당 그룹이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
 
+        shared_items = list(Item.objects.filter(group=group))
+        shared_items_data =  ItemSerializer(shared_items, many=True).data
+
+        # 유저가 이 그룹에 가입했는지 확인
         is_member = GroupMember.objects.filter(user=user, group=group).exists()
-        """ 유저가 이 그룹에 가입했는지 확인 """
+
+        # 유저가 그룹장인지 확인
         is_admin = group.group_admin == user
-        """ 유저가 그룹장인지 확인 """
 
         data = GroupSerializer(group).data
         data['is_member'] = is_member
         data['is_admin'] = is_admin
+        data['shared_items'] = shared_items_data
         return Response(data, status=status.HTTP_200_OK)
+
+class GroupManageView(APIView):
+    """ 그룹 관리(그룹 상세 정보 + 그룹 정보 편집 가능 여부 포함) """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, group_id):
+        user = request.user
+
+        try:
+            group = Group.objects.annotate(member_count=Count("members")).get(group_id=group_id)
+        except Group.DoesNotExist:
+            return Response({"message" : "해당 그룹이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 그룹장인지 확인(편집 권한 여부)
+        is_admin = group.group_admin == user
+
+        # 그룹 멤버 리스트 (4명까지만 표시)
+        members = list(GroupMember.objects.filter(group=group)[:4])
+        members_data = GroupMemberSerializer(members, many=True).data
+        total_members = GroupMember.objects.filter(group=group).count()
+
+        # 공용 물품 리스트 (5개 표시)
+        shared_items = list(Item.objects.filter(group=group)[:5])
+        shared_items_data = ItemSerializer(shared_items, many=True).data
+        total_shared_items = Item.objects.filter(group=group).count()
+
+        data = GroupSerializer(group).data
+        data['is_admin'] = is_admin
+        data['members'] = members_data
+        data['total_members'] = total_members
+        data['shared_items'] = shared_items_data
+        data['total_shared_items'] = total_shared_items
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class GroupMemberListView(APIView):
+    """ 그룹 멤버 전체 리스트 조회 """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, group_id):
+        user = request.user
+
+        try:
+            group = Group.objects.get(group_id=group_id)
+        except Group.DoesNotExist:
+            return Response({"message" : "해당 그룹이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 그룹 전체 멤버 리스트
+        members = list(GroupMember.objects.filter(group=group))
+        members_data = GroupMemberSerializer(members, many=True).data
+
+        return Response({"members" :members_data}, status=status.HTTP_200_OK)
+
+
+class GroupSharedItemListView(APIView):
+    """ 특정 그룹 내 전체 공유 물품 리스트 조회 """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, group_id):
+        user = request.user
+
+        try:
+            group = Group.objects.get(group_id=group_id)
+        except Group.DoesNotExist:
+            return Response({"message" : "해당 그룹이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 특정 그룹의 전체 공유 물품 리스트
+        shared_items = list(Item.objects.filter(group=group))
+        shared_items_data = ItemSerializer(shared_items, many=True).data
+
+        return Response({"shared_items" : shared_items_data}, status=status.HTTP_200_OK)
 
 class GroupCreateView(generics.CreateAPIView):
     """ 새로운 그룹 생성 """
