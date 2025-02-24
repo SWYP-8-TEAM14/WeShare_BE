25610e2ecc8bcbec2404eed49862021d92fc7727
benchprincess <realjiwon98@naver.com>
2025-02-24 22:27:03 +0900

    config/urls 불필요한 슬래시 제거

--- config/urls.py
+++ config/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     # apps
-    path("/api/v1/home/", HomeView.as_view(), name="home"),
+    path("api/v1/home/", HomeView.as_view(), name="home"),
     path("api/v1/users/", include("apps.users.urls")),
     path("api/v1/groups/", include("apps.groups.urls")),
     path('api/v1/shared/', include('apps.shared.urls')),
-    path("/api/v1/users/me/", UserView.as_view(), name="user"),
+    path("api/v1/users/me/", UserView.as_view(), name="user"),
 
     # Swagger 및 API 문서
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
