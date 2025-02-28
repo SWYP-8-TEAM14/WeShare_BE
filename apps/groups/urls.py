52c914b461ddbee2c2307fdcb08fffbc1d308e07
benchprincess <realjiwon98@naver.com>
2025-03-01 01:46:23 +0900

    그룹관리에서 멤버 조회하는 api에 manage/members로 restful하게 변경

--- apps/groups/urls.py
+++ apps/groups/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("groups/", GroupListView.as_view(), name="group-list"),
     path("groups/<int:group_id>/", GroupDetailView.as_view(), name="group-detail"),
     path("groups/<int:group_id>/manage/", GroupManageView.as_view(), name="group-manage"),
-    path("groups/<int:group_id>/members/", GroupMemberListView.as_view(), name="group-member-list"),
+    path("groups/<int:group_id>/manage/members/", GroupMemberListView.as_view(), name="group-member-list"),
     path("groups/<int:group_id>/items/", GroupSharedItemListView.as_view(), name="group-shared-item-list"),
     path("groups/create/", GroupCreateView.as_view(), name="group-create"),
 ]
